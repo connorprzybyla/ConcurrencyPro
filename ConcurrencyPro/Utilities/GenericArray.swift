0fdd92711558d772ff02cc1a101caa0657d3f41c
connorprzybyla <cgprzyby@umich.edu>
2022-04-30 12:32:02 -0400

    feat: add thread-safe array examples

---
+++ ConcurrencyPro/Utilities/GenericArray.swift
@@ -0,0 +1,130 @@
+//
+//  GenericArray.swift
+//  ConcurrencyPro
+//
+//  Created by Connor Przybyla on 4/29/22.
+//
+
+import Foundation
+
+/// Concurrency is a fundamental to computer science and a key aspect to performant iOS applications. Although Apple has done some excellent work to assist developers with issues relating
+/// to concurrency and multithreading, it is up to us developers to follow best practices and ensure our code is guarenteed to execute as expected even in unknown circumstances.
+/// When working with shared resources, remeber to always ask yourself, what if the run loop of the application decides it wants to perform a context switch to another thread that is accessing the same resource in memory as the previous?
+
+// MARK: - Solution #1 - DispatchQueue sync - Not Accepted
+
+/// Although the example below enforces mutual execlusion. It is most certainly not optimal. When we use sync, we are effectively blocking the current thread.
+/// We should never block the current thread unless we absolutely have to. This is the most naive approach to mutual execlusion. With a serial queue, we cannot use concurrency to
+/// imporve performance. This is a huge drawback to this implementation approach.
+
+final class GenericArrayNaive<T> {
+    private var array: [T]?
+    private let dispatchQueue = DispatchQueue(label: "generic-array-naive-example")
+    
+    init(array: [T]?) {
+        self.array = array
+    }
+    
+    func append(_ element: T) {
+        dispatchQueue.sync {
+            // Critical section
+            self.array?.append(element)
+        }
+    }
+    
+    func getValue(at index: Int) -> T? {
+        var value: T?
+        dispatchQueue.sync {
+            // Critical section
+            value = array?[index]
+        }
+        return value
+    }
+}
+
+// MARK: - Solution #2 - DispatchQueue async - Accepted
+
+/// Here we have a concurrent dispatch queue that we use to keep our critical sections protected. In the case of appending to the array, we simply wrap the append in a block that will
+/// ensure that the item will asyncronously execute at some point in time with a specific flag type `barrier`. This flag will block any thread attempting to read/write to the resources in this block.
+
+final class GenericArray<T> {
+    private var array: [T]?
+    private let dispatchQueue = DispatchQueue(label: "generic-array-example", attributes: .concurrent)
+    
+    init(array: [T]?) {
+        self.array = array
+    }
+    
+    func append(_ element: T) {
+        dispatchQueue.async(flags: .barrier) {
+            // Critical section
+            self.array?.append(element)
+        }
+    }
+    
+    func getValue(at index: Int) -> T? {
+        var value: T?
+        dispatchQueue.sync {
+            // Critical section
+            value = array?[index]
+        }
+        return value
+    }
+}
+
+// MARK: - Solution #3 - NSLock - Accepted
+
+/// Prior to Actors, NSLock was perhaps the easiest and most lightweight way to protect critical sections and enforce mutual exclusion in an iOS application.
+/// We can avoid the overhead that DispatchQueue's introduce and just rely of NSLock instead for mutual exclusion since at its core its really just a mutex. Also understand NSRecusiveLock() and why it was introduced as a way to prevent
+/// deadlocking your application when attempting to acquire a lock multiple times by the same thread. In the example below, NSLock() is all we need since the critical sections never attempt to acquire locks. It's also faster than NSRecusiveLock()
+/// so be sure to only use NSRecusiveLock() when you really need to.
+
+final class GenericArrayWithNSLock<T> {
+    private var array: [T]?
+    private var lock = NSLock()
+    
+    init(array: [T]?) {
+        self.array = array
+    }
+    
+    func append(_ element: T) {
+        lock.lock()
+        // Critical section
+        array?.append(element)
+        lock.unlock()
+    }
+    
+    func getValue(at index: Int) -> T? {
+        var value: T?
+        
+        lock.lock()
+        // Critical section
+        value = array?[index]
+        lock.unlock()
+        
+        return value
+    }
+}
+
+// MARK: - Solution #4 - Swift Actors - Accepted
+
+/// The latest and greatest from Swift 5.5. Use the keyword `actor` to enforce thread-safety. Threads are unable to access the resources in parallel.
+
+final actor GenericArrayActor<T> {
+    private var array: [T]?
+    
+    init(array: [T]?) {
+        self.array = array
+    }
+    
+    func append(_ element: T) {
+        // Critical section
+        array?.append(element)
+    }
+    
+    func getValue(at index: Int) -> T? {
+        // Critical section
+        array?[index]
+    }
+}
+
--- ConcurrencyPro/ViewController.swift
+++ ConcurrencyPro/ViewController.swift
@@ -9,27 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        return stackView
-    }()
-    
-    private let label: UILabel = {
-        let label = UILabel()
-        label.text = "Enter amount of threads"
-        return label
-    }()
-    
-    private let textField = UITextField()
-    
-    private let submitButton: UIButton = {
-        var configuration = UIButton.Configuration.gray()
-        configuration.title = "Submit"
-        let button = UIButton(configuration: configuration)
-        return button
-    }()
-    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -38,12 +17,45 @@ class ViewController: UIViewController {
         setupStackView()
     }
     
+    // MARK: - Private
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Concurrency in iOS"
+        return label
+    }()
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter number of tasks"
+        textField.layer.cornerRadius = 8
+        textField.layer.borderWidth = 0.5
+        textField.backgroundColor = .secondarySystemBackground
+        return textField
+    }()
+    
+    private let submitButton: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "Submit"
+        let button = UIButton(configuration: configuration)
+        return button
+    }()
+    
     private func setupStackView() {
         stackView.addArrangedSubview(label)
         stackView.addArrangedSubview(textField)
         stackView.addArrangedSubview(submitButton)
+        
         submitButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         view.addSubview(stackView)
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
